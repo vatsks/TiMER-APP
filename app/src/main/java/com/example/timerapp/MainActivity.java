679e5634a4e9c52847b22863186b8419ea4c8dd3
vatsks <iamksvats@gmail.com>
2021-06-26 03:05:47 +0530

    applied basic layout for the app and code for seekbar and updating the textview through seekbar

--- app/src/main/java/com/example/timerapp/MainActivity.java
+++ app/src/main/java/com/example/timerapp/MainActivity.java
@@ -14,32 +14,32 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    MediaPlayer mediaPlayer;String secondstring;
+    MediaPlayer mediaPlayer;String secondstring; SeekBar seekBar;
 // no need to call prepare(); create() does that for you
 
-    TextView textView;int time=10000;
+    TextView textView;
 
-    CountDownTimer countDownTimer=new CountDownTimer(Integer.parseInt(secondstring),1000) {
-        @Override
-        public void onTick(long millisUntilFinished) {
-
-                textView.setText(String.valueOf(time));time--;
-        }
-
-        @Override
-        public void onFinish() {
-
-                mediaPlayer.start();
-        }
-    };
 
         public  void Click(View view){
+
             countDownTimer.start();
 
 
         }
 
+        public void update(int getseconds){
 
+            int minutes=getseconds/60;
+            int seconds =getseconds-(minutes*60);
+
+            if (seconds<=9){
+                secondstring="0"+Integer.toString(seconds);
+            }else {
+                secondstring=Integer.toString(seconds);
+            }
+            textView.setText("0"+Integer.toString(minutes)+":"+ secondstring);
+
+        }
 
 
 
@@ -52,22 +52,13 @@ public class MainActivity extends AppCompatActivity {
          mediaPlayer = MediaPlayer.create(this, R.raw.alarmbeep);
 
          textView=findViewById(R.id.textView3);
-        SeekBar seekBar =findViewById(R.id.seekBar);
+         seekBar =findViewById(R.id.seekBar);
         seekBar.setMax(500);
         seekBar.setProgress(30);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                int minutes=progress/60;
-                int seconds =progress-(minutes*60);
-
-                if (seconds<=9){
-                    secondstring="0"+Integer.toString(seconds);
-                }else {
-                    secondstring=Integer.toString(seconds);
-                }
-                textView.setText("0"+Integer.toString(minutes)+":"+ secondstring);
-
+               update(progress);
 
             }
 
